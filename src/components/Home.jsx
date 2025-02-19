afafcd0262108ae59857fd92c6e3b9797b20aa69
M4ster SINISTER <91175533+MasterSINISTER@users.noreply.github.com>
2025-02-19 17:54:29 +0530

    Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./homeStyle.css";
 
 import BlurText from "./ui/BlurText/BlurText";
@@ -10,19 +10,31 @@ import Aurora from "./ui/Aurora/Aurora";
 import { useNavigate } from "react-router-dom";
 import Nav from "./Nav";
 import { gsap } from "gsap";
-    
+
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import CountUp from "react-countup";
 import { Card } from "react-bootstrap";
 import { Rating } from "@mui/material";
 function Home() {
-  
-gsap.registerPlugin(ScrollTrigger);
-  const navigate=useNavigate();
+  gsap.registerPlugin(ScrollTrigger);
+  const navigate = useNavigate();
+  const handleServicesScroll = () => {
+    const percentage = 100; // Change this to the desired percentage
+    const windowHeight = window.innerHeight;
+    const section = document.querySelector(".services-section");
+    const sectionOffsetTop = section.offsetTop;
+    const targetScrollPosition =
+      sectionOffsetTop - windowHeight * (1 - percentage / 100);
+
+    window.scrollTo({
+      top: targetScrollPosition,
+      behavior: "smooth",
+    });
+  };
   const getToStart = () => {
     navigate("/contact");
-  }
- 
+  };
+
   const images = [
     {
       id: 1,
@@ -43,27 +55,25 @@ gsap.registerPlugin(ScrollTrigger);
   ];
 
   useEffect(() => {
-    gsap.to(".lawyer-img",{
-      scrollTrigger:{
-        trigger:".lawyer-img",
-        toggleActions:"play pause resume restart",
-        scrub:2,
+    gsap.to(".lawyer-img", {
+      scrollTrigger: {
+        trigger: ".lawyer-img",
+        toggleActions: "play pause resume restart",
+        scrub: 2,
         // markers:true,
-        },
-      x:240,
-      duration:2,
-  
+      },
+      x: 240,
+      duration: 2,
     });
-    gsap.to(".person-img",{
-      scrollTrigger:{
-        trigger:".person-img",
-        toggleActions:"play pause resume restart",
-        scrub:2,
-        },
-      translateX:-240,
-      duration:2,
+    gsap.to(".person-img", {
+      scrollTrigger: {
+        trigger: ".person-img",
+        toggleActions: "play pause resume restart",
+        scrub: 2,
+      },
+      translateX: -240,
+      duration: 2,
       // ease:"none",
-  
     });
     gsap.utils.toArray(".firstSection-heading").forEach((heading) => {
       gsap.to(heading, {
@@ -93,17 +103,17 @@ gsap.registerPlugin(ScrollTrigger);
       opacity: 1,
       color: "grey",
       duration: 2,
-    })
+    });
 
-    gsap.to(".para-style",{
-      scrollTrigger:{
-        trigger:".para-style",
-        toggleActions:"play pause resume restart",
-        scrub:1,
-        },
-      opacity:1,
-      duration:2,
-    })
+    gsap.to(".para-style", {
+      scrollTrigger: {
+        trigger: ".para-style",
+        toggleActions: "play pause resume restart",
+        scrub: 1,
+      },
+      opacity: 1,
+      duration: 2,
+    });
   }, []);
 
   const [showModal, setShowModal] = useState(false);
@@ -125,24 +135,35 @@ gsap.registerPlugin(ScrollTrigger);
         <div className="modal">
           <div className="modal-content">
             <h2>Disclaimer</h2>
-            <p>As per the rules of the Bar Council of India, we are not permitted to solicit work and advertise. By visiting the website, the user acknowledges that the information provided on this website is solely available for informational purposes only sought to be voluntarily gained by him/her and is neither soliciting nor advertisement. Further, the information provided on this website is accessed by the user’s own volition, and any transmission, receipt or use of this information available on this website does not create any liability or any relationship with us.</p>
-            <button onClick={handleAccept} className="btn-accept">Accept</button>
+            <p>
+              As per the rules of the Bar Council of India, we are not permitted
+              to solicit work and advertise. By visiting the website, the user
+              acknowledges that the information provided on this website is
+              solely available for informational purposes only sought to be
+              voluntarily gained by him/her and is neither soliciting nor
+              advertisement. Further, the information provided on this website
+              is accessed by the user’s own volition, and any transmission,
+              receipt or use of this information available on this website does
+              not create any liability or any relationship with us.
+            </p>
+            <button onClick={handleAccept} className="btn-accept">
+              Accept
+            </button>
           </div>
         </div>
       )}
 
       <div className="top-container">
-      
- <Nav/>
+        <Nav />
         <div className="video-container">
-        <Aurora
-  colorStops={["#00D8FF", "#00D8FF", "#00D8FF"]}
-  speed={1}
-  className="aurora"
-/>
-       
+          <Aurora
+            colorStops={["#00D8FF", "#00D8FF", "#00D8FF"]}
+            speed={1}
+            className="aurora"
+          />
+
           {/* <Spline scene="https://prod.spline.design/akjlNvwd3otKrzNA/scene.splinecode" /> */}
-        {/* <img src="https://i.postimg.cc/qvbLWvZs/blk5.jpg" className="image-background" alt="spline"></img> */}
+          {/* <img src="https://i.postimg.cc/qvbLWvZs/blk5.jpg" className="image-background" alt="spline"></img> */}
           <div className="heading-container">
             <div>
               <BlurText
@@ -159,57 +180,55 @@ gsap.registerPlugin(ScrollTrigger);
                 direction="top"
                 className="heading-sub"
               />
-          
-             <div style={{display:"flex",flexDirection:"row"}} className="switcher-div" >
-             <h1 className="sub-heading" >We Help You with </h1>
-             <RotatingText
-                texts={['Legal Matters', 'Disputes', 'Claims', 'Justice!']}
-                mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
-                staggerFrom={"last"}
-                initial={{ y: "100%",opacity: 0 }}
-                animate={{ y: 0 ,opacity: 1}}
-                exit={{ y: "-100%",opacity: 0 }}
-                staggerDuration={0.025}
-                splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-                splitBy="characters"
-                transition={{ type: "spring", damping: 40, stiffness: 400 }}
-                rotationInterval={3500}
-                loop={false}
-                className="heading-switch"              />
-             </div>
-              
-        <button className="btn-explore" onClick={getToStart}>Get Assistance !</button>
+
+              <div
+                style={{ display: "flex", flexDirection: "row" }}
+                className="switcher-div"
+              >
+                <h1 className="sub-heading">We Help You with </h1>
+                <RotatingText
+                  texts={["Legal Matters", "Disputes", "Claims", "Justice!"]}
+                  mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+                  staggerFrom={"last"}
+                  initial={{ y: "100%", opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  exit={{ y: "-100%", opacity: 0 }}
+                  staggerDuration={0.025}
+                  splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                  splitBy="characters"
+                  transition={{ type: "spring", damping: 40, stiffness: 400 }}
+                  rotationInterval={3500}
+                  loop={false}
+                  className="heading-switch"
+                />
+              </div>
+
+              <button className="btn-explore" onClick={handleServicesScroll}>
+                Explore Now !
+              </button>
             </div>
           </div>
         </div>
       </div>
 
       <section className="about-us">
-    
-        <h1 className="firstSection-heading">
-            Legal Assistance at Right Time
-          </h1>
-      
-        <div
-          className="firstSection-container"
-        >
+        <h1 className="firstSection-heading">Legal Assistance at Right Time</h1>
 
+        <div className="firstSection-container">
           <div className="para-container">
-          <div
-     className="img-container"
->
-  <img
-    src="https://i.postimg.cc/266fhMyY/2330082.webp"
-    alt="lawyerImage"
-    className="lawyer-img"
-  />
+            <div className="img-container">
+              <img
+                src="https://i.postimg.cc/266fhMyY/2330082.webp"
+                alt="lawyerImage"
+                className="lawyer-img"
+              />
 
-  <img
-    src="https://i.postimg.cc/pdkH9R2k/4538053.webp"
-    alt="lawyerImage"
-    className="person-img"
-  />
-</div>
+              <img
+                src="https://i.postimg.cc/pdkH9R2k/4538053.webp"
+                alt="lawyerImage"
+                className="person-img"
+              />
+            </div>
             <p className="para-style">
               {/* Advocate R. K. Gupta has been practicing and handling cases
               independently with a result oriented approach, both professionally
@@ -231,19 +250,25 @@ gsap.registerPlugin(ScrollTrigger);
               been looking matters in M .P. High Court, Indore bench, District
               and Session court and Family court ,Indore and its surrounded
               areas. */}
-
-Legal assistance at the right time can protect your rights, prevent complications, and ensure fair outcomes. Whether it's a criminal case, civil dispute, business matter, or employment issue, timely legal guidance can save you from unnecessary stress and financial loss. Consulting a lawyer before signing contracts, handling disputes, or facing legal action helps you stay compliant with the law and make informed decisions. From protecting businesses to defending individuals, professional legal support ensures justice and peace of mind. Don't wait for problems to escalate—seek legal help when you need it the most.
+              Legal assistance at the right time can protect your rights,
+              prevent complications, and ensure fair outcomes. Whether it's a
+              criminal case, civil dispute, business matter, or employment
+              issue, timely legal guidance can save you from unnecessary stress
+              and financial loss. Consulting a lawyer before signing contracts,
+              handling disputes, or facing legal action helps you stay compliant
+              with the law and make informed decisions. From protecting
+              businesses to defending individuals, professional legal support
+              ensures justice and peace of mind. Don't wait for problems to
+              escalate—seek legal help when you need it the most.
               <br></br>
               <br></br>
               <br></br>
             </p>
-          
           </div>
         </div>
       </section>
 
-      <section className="services-section" >
-
+      <section className="services-section">
         <h1 className="firstSection-heading">Our Services</h1>
         {/* <div className="services-container">
         <Spline scene="https://prod.spline.design/HNIw2OUmhoGcqraf/scene.splinecode" />
@@ -318,7 +343,9 @@ Legal assistance at the right time can protect your rights, prevent complication
             showTooltip={true}
             displayOverlayContent={true}
             overlayContent={
-              <p className="tilted-card-demo-text">MOTOR ACCIDENT CLAIM CASES</p>
+              <p className="tilted-card-demo-text">
+                MOTOR ACCIDENT CLAIM CASES
+              </p>
             }
           />
           <TiltedCard
@@ -385,9 +412,10 @@ Legal assistance at the right time can protect your rights, prevent complication
             showMobileWarning={false}
             showTooltip={true}
             displayOverlayContent={true}
-
             overlayContent={
-              <p className="tilted-card-demo-text">TRANSFER PETITION IN SUPREME COURT</p>
+              <p className="tilted-card-demo-text">
+                TRANSFER PETITION IN SUPREME COURT
+              </p>
             }
           />
           <TiltedCard
@@ -409,46 +437,74 @@ Legal assistance at the right time can protect your rights, prevent complication
           />
         </div>
       </section>
-      <section
-        className="our-services"
-      >
+      <section className="our-services">
         <h1 className="firstSection-heading">Testimonials</h1>
         <div style={{ marginTop: "10%" }}>
-          <div className="tes-cards-container" data-aos="slide-right" delay={1000}>
+          <div
+            className="tes-cards-container"
+            data-aos="slide-right"
+            delay={1000}
+          >
             <div className="tes-card">
-              <img src="https://assets-v2.lottiefiles.com/a/72d1e8f4-1169-11ee-be4e-f75a6faac104/K5i1Cfk8j5.gif" alt="" className="img-avatar"/>
-            <br></br>
-            <br></br>
-<Rating name="size-small" defaultValue={5}  readOnly size="Medium" sx={{ color: "white" }}/>
-              <h2 className="tes-heading">
-              Akanksha
-              </h2>
-              <h3 className="tes-content">
-              Consultation was nice. I am satisfied with it.              </h3>
-              </div>
-              <div className="tes-card">
-              <img src="https://assets-v2.lottiefiles.com/a/82411e66-1184-11ee-8cfa-d707e53cae38/bccvxj7Ogv.gif" alt="" />
+              <img
+                src="https://assets-v2.lottiefiles.com/a/72d1e8f4-1169-11ee-be4e-f75a6faac104/K5i1Cfk8j5.gif"
+                alt=""
+                className="img-avatar"
+              />
               <br></br>
-            <br></br>
-<Rating name="size-small" defaultValue={5}  readOnly size="Medium" sx={{ color: "white" }}/>
-              <h2 className="tes-heading">
-              Nitin
-              </h2>
-              <h3 className="tes-content">
-              The advise provided by advocate was very helpful.</h3>
-              </div>
-              <div className="tes-card">
-              
-              <img src="https://cdn.dribbble.com/users/456158/screenshots/6305721/care-bot.gif" alt="" />
               <br></br>
-            <br></br>
-<Rating name="size-small" defaultValue={5} readOnly size="Medium" sx={{ color: "white" }}/>
-              <h2 className="tes-heading">
-              Anindita 
-              </h2>
+              <Rating
+                name="size-small"
+                defaultValue={5}
+                readOnly
+                size="Medium"
+                sx={{ color: "white" }}
+              />
+              <h2 className="tes-heading">Akanksha</h2>
               <h3 className="tes-content">
-              Thanks for recommending Advocate Gupta. A genuine expert, with impressive knowledge and experience. He helped us solve our problem in a warm and unassuming manner.              </h3>
-              </div>
+                Consultation was nice. I am satisfied with it.{" "}
+              </h3>
+            </div>
+            <div className="tes-card">
+              <img
+                src="https://assets-v2.lottiefiles.com/a/82411e66-1184-11ee-8cfa-d707e53cae38/bccvxj7Ogv.gif"
+                alt=""
+              />
+              <br></br>
+              <br></br>
+              <Rating
+                name="size-small"
+                defaultValue={5}
+                readOnly
+                size="Medium"
+                sx={{ color: "white" }}
+              />
+              <h2 className="tes-heading">Nitin</h2>
+              <h3 className="tes-content">
+                The advise provided by advocate was very helpful.
+              </h3>
+            </div>
+            <div className="tes-card">
+              <img
+                src="https://cdn.dribbble.com/users/456158/screenshots/6305721/care-bot.gif"
+                alt=""
+              />
+              <br></br>
+              <br></br>
+              <Rating
+                name="size-small"
+                defaultValue={5}
+                readOnly
+                size="Medium"
+                sx={{ color: "white" }}
+              />
+              <h2 className="tes-heading">Anindita</h2>
+              <h3 className="tes-content">
+                Thanks for recommending Advocate Gupta. A genuine expert, with
+                impressive knowledge and experience. He helped us solve our
+                problem in a warm and unassuming manner.{" "}
+              </h3>
+            </div>
           </div>
           {/* <div className="stack-container" data-aos="fade-in">
           <Stack
@@ -462,62 +518,55 @@ Legal assistance at the right time can protect your rights, prevent complication
           </div> */}
           <div className="stats-container" data-aos="fade-in">
             <div>
-            <CountUp
-              to={4025}
-              start={0}
-              end={4025}
-              duration={40}
-              separator=","
-              startWhen={true}
-            />
-            <br>
-            </br>
-            <label>Successful Cases</label>
+              <CountUp
+                to={4025}
+                start={0}
+                end={4025}
+                duration={40}
+                separator=","
+                startWhen={true}
+              />
+              <br></br>
+              <label>Successful Cases</label>
             </div>
-            
+
             <div>
-            <CountUp
-              to={3270}
-              start={0}
-              end={3270}
-              duration={40}
-              separator=","
-              startWhen={true}
-            />
-            <br>
-            </br>
-            <label>Trusted Clients</label>
+              <CountUp
+                to={3270}
+                start={0}
+                end={3270}
+                duration={40}
+                separator=","
+                startWhen={true}
+              />
+              <br></br>
+              <label>Trusted Clients</label>
             </div>
             <div>
-            <CountUp
-              to={6349}
-              start={0}
-              end={6349}
-              duration={40}
-              separator=","
-              startWhen={true}
-            />
-            <br>
-            </br>
-            <label>Case Studies</label>
+              <CountUp
+                to={6349}
+                start={0}
+                end={6349}
+                duration={40}
+                separator=","
+                startWhen={true}
+              />
+              <br></br>
+              <label>Case Studies</label>
             </div>
             <div>
-            <CountUp
-              to={851}
-              start={0}
-              end={851}
-              duration={40}
-              separator=","
-              startWhen={true}
-            />
-            <br>
-            </br>
-            <label>Registered Case</label>
+              <CountUp
+                to={851}
+                start={0}
+                end={851}
+                duration={40}
+                separator=","
+                startWhen={true}
+              />
+              <br></br>
+              <label>Registered Case</label>
             </div>
-            
-            
           </div>
-         
         </div>
       </section>
       <footer>
--- src/components/NewCard.jsx
+++ src/components/NewCard.jsx
@@ -1,27 +1,19 @@
-import React from 'react'
-import './newCardStyle.css'
+import React from "react";
+import "./newCardStyle.css";
 
 function NewCard(props) {
   return (
     <>
-           
-        <div class="cord">
-            <h1>
-                <span class="text-num">
-{props.num}
-
-</span>
-            </h1>
-            <h3>
-                <span class="text">
-{props.desc}
-
-</span>
-            </h3>
-        </div>
-
+      <div class="cord">
+        <h1>
+          <span class="text-num">{props.num}</span>
+        </h1>
+        <h3>
+          <span class="text">{props.desc}</span>
+        </h3>
+      </div>
     </>
-  )
+  );
 }
 
-export default NewCard
+export default NewCard;
